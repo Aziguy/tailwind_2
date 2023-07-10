0c2a9b4926b8d27861dd699ac49af778340247f5
Aziguy <monsieur_hernandez@hotmail.com>
2023-07-10 11:18:03 +0200

    Extract images from Figma and build a landing page.

---
+++ my-preset.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                comf: ['Comfortaa', 'sans-serif'],
+                lora: ['Lora', 'serif'],
+                nun: ['Nunito', 'sans-serif'],
+                libre: ['Libre Baskerville', 'serif'],
+                monte: ["Montserrat", "sans-serif"],
+            },
+        },
+    },
+}
